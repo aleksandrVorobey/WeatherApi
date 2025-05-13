7352928d9f58faac4532713a20fa79bfbe3ff548
Aleksandr Vorobei <vorobeisasa@gmail.com>
2025-05-13 12:08:25 +0300

    start project

---
+++ WhetherApi/WhetherApi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WhetherApi
+//
+//  Created by Александр Воробей on 13.05.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
